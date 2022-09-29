3b8f9665a3e78306c5748cff946999e5bc12ab78
kobenan <kkhoumar@gmail.com>
2022-09-29 01:17:37 +0100

    projet de fin d'etude

---
+++ resources/views/accueil.blade.php
@@ -0,0 +1,336 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Venue - Responsive Web Template</title>
+        
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="css/fontAwesome.css">
+        <link rel="stylesheet" href="css/hero-slider.css">
+        <link rel="stylesheet" href="css/owl-carousel.css">
+        <link rel="stylesheet" href="css/datepicker.css">
+        <link rel="stylesheet" href="css/templatemo-style.css">
+
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+
+        <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+<!--
+	Venue Template
+	http://www.templatemo.com/tm-522-venue
+-->
+    </head>
+
+<body>
+ 
+    <div class="wrap">
+        <header id="header">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-12" style="background-color:#21008c;
+                    color:#fff;size:50px">
+                        <button id="primary-nav-button" type="button">Menu</button>
+                        <a href="index.html"><div class="logo">
+                            <img src="" alt="Venue Logo">
+                        </div></a>
+                        <nav id="primary-nav" class="dropdown cf">
+                            <ul class="dropdown menu">
+                                <li class='active'><a href="#">Immobilier NEUF</a></li>
+                                <li><a href="#">Langue</a>
+                                    <ul class="sub-menu">
+                                        <li style="background-color:#21008c;">
+                                        <a href="#">français</a>
+                                        <a href="#">anglais</a>
+                                        <a href="#">espagnol</a>
+                                            
+                                        </li>
+                
+                                    </ul>
+                                </li>
+                                <li><a class="scrollTo" data-scrollTo="blog" href="#">Blog</a></li>
+                                <li><a class="scrollTo" data-scrollTo="services" href="#">Connecter</a></li>
+                                <li><a style="color:#ff6600;font-size:15px;" href="" class="btn" rel="follow" title="Publier une annonce">Publier une annonce</a></li>
+                            </ul>
+                        </nav><!-- / #primary-nav -->
+                    </div>
+                </div>
+            </div>
+        </header>
+    </div>
+      
+
+    
+    <section class="banner" id="top">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1">
+                    <div class="banner-caption">
+                        <div class="line-dec"></div>
+                        <h2>Best Finder For You</h2>
+                        <span>Suspendisse eu lorem massa. Integer sit amet posuere tellus.</span>
+                        <div class="blue-button">
+                            <a class="scrollTo" data-scrollTo="popular" href="#">Discover More</a>
+                        </div>
+                    </div>
+                    <div class="submit-form">
+                        <form id="form-submit" action="" method="get">
+                            <div class="row">
+                                <div class="col-md-3 first-item">
+                                    <fieldset>
+                                        <input name="name" type="text" class="form-control" id="name" placeholder="Your name..." required="">
+                                    </fieldset>
+                                </div>
+                                <div class="col-md-3 second-item">
+                                    <fieldset>
+                                        <input name="location" type="text" class="form-control" id="location" placeholder="Type location..." required="">
+                                    </fieldset>
+                                </div>
+                                <div class="col-md-3 third-item">
+                                    <fieldset>
+                                        <select required name='category' onchange='this.form.()'>
+                                            <option value="">Select category...</option>
+                                            <option value="Shops">Shops</option>
+                                            <option value="Hotels">Hotels</option>
+                                            <option value="Restaurants">Restaurants</option>
+                                            <option value="Events">Events</option>
+                                            <option value="Meetings">Meetings</option>
+                                            <option value="Fitness">Fitness</option>
+                                            <option value="Cafes">Cafes</option>
+                                        </select>
+                                    </fieldset>
+                                </div>
+                                <div class="col-md-3">
+                                    <fieldset>
+                                        <button type="submit" id="form-submit" class="btn">Search Now</button>
+                                    </fieldset>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+<!-- espace libre -->
+
+
+    <section class="featured-places" id="blog">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="section-heading">
+                        <span>Featured Places</span>
+                        <h2>Praesent nec dui sed urna</h2>
+                    </div>
+                </div> 
+            </div> 
+            <div class="row">
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                    <div class="featured-item">
+                        <div class="thumb">
+                            <img src="img/featured_item_1.jpg" alt="">
+                            <div class="overlay-content">
+                                <ul>
+                                    <li><i class="fa fa-star"></i></li>
+                                    <li><i class="fa fa-star"></i></li>
+                                    <li><i class="fa fa-star"></i></li>
+                                    <li><i class="fa fa-star"></i></li>
+                                    <li><i class="fa fa-star"></i></li>
+                                </ul>
+                            </div>
+                            <div class="date-content">
+                                <h6>28</h6>
+                                <span>August</span>
+                            </div>
+                        </div>
+                        <div class="down-content">
+                            <h4>Lorem ipsum dolor</h4>
+                            <span>Category One</span>
+                            <p>Vestibulum id est eu felis vulputate hendrerit. Suspendisse dapibus turpis in dui pulvinar imperdiet. Nunc consectetur.</p>
+                            <div class="row">
+                                <div class="col-md-6 first-button">
+                                    <div class="text-button">
+                                        <a href="#">Add to favorites</a>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="text-button">
+                                        <a href="#">Continue Reading</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                    <div class="featured-item">
+                        <div class="thumb">
+                            <img src="img/featured_item_2.jpg" alt="">
+                            <div class="overlay-content">
+                                <ul>
+                                    <li><i class="fa fa-star"></i></li>
+                                    <li><i class="fa fa-star"></i></li>
+                                    <li><i class="fa fa-star"></i></li>
+                                    <li><i class="fa fa-star"></i></li>
+                                    <li><i class="fa fa-star"></i></li>
+                                </ul>
+                            </div>
+                            <div class="date-content">
+                                <h6>20</h6>
+                                <span>September</span>
+                            </div>
+                        </div>
+                        <div class="down-content">
+                            <h4>Nullam nibh lacus</h4>
+                            <span>Category Two</span>
+                            <p>Mauris sit amet quam congue, pulvinar urna et, congue diam. Suspendisse eu lorem massa. Integer sit amet posuere.</p>
+                            <div class="row">
+                                <div class="col-md-6 first-button">
+                                    <div class="text-button">
+                                        <a href="#">Add to favorites</a>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="text-button">
+                                        <a href="#">Continue Reading</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                    <div class="featured-item">
+                        <div class="thumb">
+                            <img src="img/featured_item_3.jpg" alt="">
+                            <div class="overlay-content">
+                                <ul>
+                                    <li><i class="fa fa-star"></i></li>
+                                    <li><i class="fa fa-star"></i></li>
+                                    <li><i class="fa fa-star"></i></li>
+                                    <li><i class="fa fa-star"></i></li>
+                                    <li><i class="fa fa-star"></i></li>
+                                </ul>
+                            </div>
+                            <div class="date-content">
+                                <h6>12</h6>
+                                <span>October</span>
+                            </div>
+                        </div>
+                        <div class="down-content">
+                            <h4>Suspendisse semper non</h4>
+                            <span>Category Three</span>
+                            <p>Praesent iaculis gravida elementum. Proin fermentum neque facilisis semper pharetra. Sed vestibulum vehicula tincidunt.</p>
+                            <div class="row">
+                                <div class="col-md-6 first-button">
+                                    <div class="text-button">
+                                        <a href="#">Add to favorites</a>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="text-button">
+                                        <a href="#">Continue Reading</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+  
+
+
+
+   
+
+
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-5">
+                    <div class="about-veno">
+                        <div class="logo">
+                            <img src="img/footer_logo.png" alt="Venue Logo">
+                        </div>
+                        <p>Mauris sit amet quam congue, pulvinar urna et, congue diam. Suspendisse eu lorem massa. Integer sit amet posuere tellus, id efficitur leo. In hac habitasse platea dictumst.</p>
+                        <ul class="social-icons">
+                            <li>
+                                <a href="#"><i class="fa fa-facebook"></i></a>
+                                <a href="#"><i class="fa fa-twitter"></i></a>
+                                <a href="#"><i class="fa fa-linkedin"></i></a>
+                                <a href="#"><i class="fa fa-rss"></i></a>
+                                <a href="#"><i class="fa fa-dribbble"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="useful-links">
+                        <div class="footer-heading">
+                            <h4>Useful Links</h4>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <ul>
+                                    <li><a href="#"><i class="fa fa-stop"></i>Help FAQs</a></li>
+                                    <li><a href="#"><i class="fa fa-stop"></i>Register</a></li>
+                                    <li><a href="#"><i class="fa fa-stop"></i>Login</a></li>
+                                    <li><a href="#"><i class="fa fa-stop"></i>My Profile</a></li>
+                                    <li><a href="#"><i class="fa fa-stop"></i>How It Works?</a></li>
+                                    <li><a href="#"><i class="fa fa-stop"></i>More About Us</a></li>
+                                </ul>
+                            </div>
+                            <div class="col-md-6">
+                                <ul>
+                                    <li><a href="#"><i class="fa fa-stop"></i>Our Clients</a></li>
+                                    <li><a href="#"><i class="fa fa-stop"></i>Partnerships</a></li>
+                                    <li><a href="#"><i class="fa fa-stop"></i>Blog Entries</a></li>
+                                    <li><a href="#"><i class="fa fa-stop"></i>Contact Us</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="contact-info">
+                        <div class="footer-heading">
+                            <h4>Contact Information</h4>
+                        </div>
+                        <p>Praesent iaculis gravida elementum. Proin fermentum neque facilisis semper pharetra. Sed vestibulum vehicula tincidunt.</p>
+                        <ul>
+                            <li><span>Phone:</span><a href="#">010-050-0550</a></li>
+                            <li><span>Email:</span><a href="#">hi@company.co</a></li>
+                            <li><span>Address:</span><a href="#">company.co</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <div class="sub-footer">
+        <p>Copyright &copy; 2018 Company Name 
+    
+    	- Design: <a rel="nofollow" href="http://www.templatemo.com">Template Mo</a></p>
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js" type="text/javascript"></script>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+
+    <script src="js/vendor/bootstrap.min.js"></script>
+    
+    <script src="js/datepicker.js"></script>
+    <script src="js/plugins.js"></script>
+    <script src="js/main.js"></script>
+</body>
+</html>
